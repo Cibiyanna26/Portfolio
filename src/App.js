e649a335e67a69349df5bfca1792856d10355882
Cibiyanna <cibiyanna.p2022cse@sece.ac.in>
2023-11-17 01:33:39 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Contact from './components/Contact';
+import Nav from './components/Nav';
+import About from './components/About';
+import Service from './components/Services';
+import Work from './components/Work';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='bg-site bg-no-repeat bg-cover overflow-hidden text-white font-primary'>
+        <Header/>
+        <Banner/>
+        <Nav/>
+        <About/>
+        {/* <Service/> */}
+        <Work/>
+        <Contact/>
+        
+      </div>
+    </>
+
+  )
 }
 
 export default App;
